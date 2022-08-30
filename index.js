255491db7e40a9aa7d8728d08e7c7d08c9a58716
Chris Helder <46414358+TheDude53@users.noreply.github.com>
2022-08-30 01:01:04 -0500

    Remove "cool" retro flicker
    It's just going to end up giving you a seizure.

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 "use strict";
 
 function print(html) {
-  document.body.className = "blur";
   document.body.innerHTML += html.replace(/\n/g, "<br>");
-  setTimeout(()=>{
-    // The delay gives a nice retro flicker
-    document.body.className = "";
-  }, 50);
   // Scroll to bottom
   window.scroll(window.scrollX, document.body.scrollHeight);
 };
